b97e6af36f401c7f8506fc128b37086f2c6ba844
Juan Formoso <trajano.juan@gmail.com>
2021-07-09 15:48:01 -0400

    Tentando finalizar requisito 6 pela 8° vez

---
+++ script.js
@@ -0,0 +1,5 @@
+let paletteColors = document.getElementsByClassName('color');
+paletteColors[0].style.backgroundColor = 'black';
+paletteColors[1].style.backgroundColor = '#34568b';
+paletteColors[2].style.backgroundColor = '#ff6f61';
+paletteColors[3].style.backgroundColor = '#88b04b';
