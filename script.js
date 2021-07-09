3f1fe3756f08df91fb8eb9f647a2589aed83302e
Juan Formoso <trajano.juan@gmail.com>
2021-07-09 02:08:59 -0400

    Tentando concertar erros novamente

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ const colors = document.querySelectorAll('.color');
 const divColors = document.body.children[1];
 
 function selectedPallete(event) {
-    for (let index = 0; index < colors.length; index += 1) {
-      if (colors[index].classList.length === 3) {
-        event.target.parentElement.children[index].classList.remove('selected');
-      }
-      if (colors[index].classList.length === 2) {
-        event.target.classList.add('selected');
-      }
+  for (let index = 0; index < colors.length; index += 1) {
+    if (colors[index].classList.length === 3) {
+      event.target.parentElement.children[index].classList.remove('selected');
     }
+    if (colors[index].classList.length === 2) {
+      event.target.classList.add('selected');
+    }
+    
   }
+}
   
-  divColors.addEventListener('click', selectedPallet);
+divColors.addEventListener('click', selectedPallete);
