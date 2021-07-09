a56c8ed2122f816acfa1158e0a7779b0c3ce17eb
Juan Formoso <trajano.juan@gmail.com>
2021-07-09 02:05:41 -0400

    Tentando finalizar requisito 7 e 4

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ window.addEventListener('load', function() {
   black.classList.add('selected')
 })
 
-let colors = document.querySelectorAll('.color');
-let paletteColors = document.body.children[1];
+const colors = document.querySelectorAll('.color');
+const divColors = document.body.children[1];
 
-function selectedColor(event) {
-  for (let index = 0; index < colors.length; index += 1) {
-    if (colors[index].classList.length === 3) {
-      event.target.parentElement.children[index].classList.remove('selected');
-    }
-    if (colors[index].classList.length === 2) {
-      event.target.classList.add('selected');
+function selectedPallete(event) {
+    for (let index = 0; index < colors.length; index += 1) {
+      if (colors[index].classList.length === 3) {
+        event.target.parentElement.children[index].classList.remove('selected');
+      }
+      if (colors[index].classList.length === 2) {
+        event.target.classList.add('selected');
+      }
     }
   }
-}
-
-paletteColors.addEventListener('click', selectedColor);
+  
+  divColors.addEventListener('click', selectedPallet);
