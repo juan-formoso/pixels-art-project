15bb11f085c7d906f48282c590c115900597f2bc
Juan Formoso <trajano.juan@gmail.com>
2021-07-09 01:46:34 -0400

    Finalizando requisito 7-2

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const black = document.querySelector('.black');
 window.addEventListener('load', function() {
   black.classList.add('selected')
 })
+
+let colors = document.querySelectorAll('.color');
+let paletteColors = document.body.children[1];
+
+function selectedColor(event) {
+  for (let index = 0; index < colors.length; index += 1) {
+    if (colors[index].classList.length === 3) {
+      event.target.parentElement.children[index].classList.remove('selected');
+    }
+    if (colors[index].classList.length === 2) {
+      event.target.classList.add('selected');
+    }
+  }
+}
+
+paletteColors.addEventListener('click', selectedColor);
