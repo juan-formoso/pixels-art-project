41331492fc45966c272ca0a46fd577a5f7fa0604
Juan Formoso <trajano.juan@gmail.com>
2021-07-09 02:13:48 -0400

    Tentando consertar erros novamente parte 3

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const black = document.querySelector('.black');
 
 window.addEventListener('load', function() {
-  black.classList.add('selected')
+  black.classList.add('selected');
 })
 
-const colors = document.querySelectorAll('.color');
-const divColors = document.body.children[1];
+let colors = document.querySelectorAll('.color');
+let divColors = document.body.children[0].children[1];
+let selectColor = document.querySelector('.selected');
 
-function selectedPallete(event) {
+function selectedPallet(event) {
   for (let index = 0; index < colors.length; index += 1) {
     if (colors[index].classList.length === 3) {
       event.target.parentElement.children[index].classList.remove('selected');
@@ -15,8 +16,7 @@ function selectedPallete(event) {
     if (colors[index].classList.length === 2) {
       event.target.classList.add('selected');
     }
-    
   }
 }
-  
-divColors.addEventListener('click', selectedPallete);
+
+divColors.addEventListener('click', selectedPallet);
